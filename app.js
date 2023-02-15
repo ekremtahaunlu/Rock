0d5b44e6ca55d678fce36e45c7137408b8a0d24a
Ekrem Taha Ünlü <104407089+ekremtahaunlu@users.noreply.github.com>
2023-02-15 17:49:57 +0300

    Added css and js

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
-function getComputerChoice() {}
+const choices = ["rock", "paper", "scissors"];
+
+function getComputerChoice() {
+  return choices[Math.floor(Math.random() * choices.length)];
+}
+
+function play(userChoice) {
+  const computer = getComputerChoice();
+  if (userChoice === computer) {
+    return "It's a tie!";
+  } else if (
+    (userChoice === "rock" && computer === "scissors") ||
+    (userChoice === "paper" && computer === "rock") ||
+    (userChoice === "scissors" && computer === "paper")
+  ) {
+    return "You win!";
+  } else {
+    return "Computer wins!";
+  }
+}
+
+document.getElementById("rock").addEventListener("click", function() {
+  const result = play("rock");
+  document.getElementById("result").innerHTML = result;
+});
+
+document.getElementById("paper").addEventListener("click", function() {
+  const result = play("paper");
+  document.getElementById("result").innerHTML = result;
+});
+
+document.getElementById("scissors").addEventListener("click", function() {
+  const result = play("scissors");
+  document.getElementById("result").innerHTML = result;
+});
